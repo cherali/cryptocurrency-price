ab524f172dbf50ba0a37894cff0f2e178c259471
alich6 <alicheraghi148@gmail.com>
2023-02-14 22:29:33 +0330

    next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
+  optimizeFonts: true,
+  productionBrowserSourceMaps: true,
+  compiler: {
+    styledComponents: true,
+    reactRemoveProperties: true,
+    removeConsole: process.env.NODE_ENV === 'production' && {
+      exclude: ['error'],
+    },
+  },
 }
 
 module.exports = nextConfig
